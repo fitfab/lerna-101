d2523a39f3aa80946d60427b5e1ea6e4be176efa
Miguel Julio <fitfab@yahoo.com>
2024-02-25 17:08:12 -0500

    feat: add variant for button

--- packages/button/lib/Button.tsx
+++ packages/button/lib/Button.tsx
@@ -3,7 +3,7 @@ import React from "react"
  * Ref: https://www.totaltypescript.com/concepts/react-componentprops-type-helper
  */
 export interface Buttonprops extends React.ComponentProps<"button"> {
-  action?: string
+  variant?: "primary" | "secondary" | "outline"
 }
 export function Button({ children, ...restProps }: Buttonprops) {
   return <button {...restProps}>{children}</button>
